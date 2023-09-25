52155082da6249b48ecf90a4f990d64f4b83ee8d
Marcus Gugacs <iimpaq@proton.me>
2023-09-25 14:42:12 +0200

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Marcus Gugacs on 25.09.23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
